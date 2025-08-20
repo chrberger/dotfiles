d5d819228aee6c71291dc2faa944363347546ff8
Christian Berger <christian.berger@gu.se>
2025-08-20 13:47:20 +0200

    adjusting fzf default
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -4,4 +4,6 @@ if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+
 source <(fzf --zsh)
